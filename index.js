72b19fb1bde22fb7686c2bdd557a54f0f661c737
SheharyarEkkel <sheharyar.khan@ekkel.ai>
2023-05-31 14:46:34 +0500

    testing

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("Working");
+const express = require('express')
+const app = express()
+const port = 3010
+
+app.post('/', (req, res) => {
+    console.log(req.body,"working")
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
